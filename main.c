96cb7bd115ceed2c8ed7fef21c4f2aef03474458
HeshamTB <hishaminv@gmail.com>
2021-04-20 05:09:31 +0300

    Major rework:
            Reworked input system. New flag 'new_char'
            is used in main loop to run the actual program
            and let interrupts only handle reading the input.
    
            Disable interrupts when a new char is observed
            to prevent overlap.
    
    	Still does not reset after first try or check
    	the password.
    
    Signed-off-by: HeshamTB <hishaminv@gmail.com>

--- main.c
+++ main.c
@@ -8,58 +8,66 @@
 
 #include <xc.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+
 #include "config.h"
 #include "lcd.h"
 
 #define MOTD_S "Vault System"
-#define PASSWD_LENGTH 4
 
+#define PORTB_NOMINAL 0xF0
+
+#define PASSWD_LENGTH 5 // Pass + null char
 #define PASSWD "1234"
 
 void setup();
-void get_input();
+char get_input_char();
+void keypad_isr();
 
-short letters = PASSWD_LENGTH;
+short letters = 0;
 char input[PASSWD_LENGTH];
-
-char const keyPadMatrix[] =
-{
-    '1','2','3',
-    '4','5','6',
-    '7','8','9',
-    '*','0','#',
-    0xFF
-};
+short new_char = 0;
  
 
 void __interrupt() interrupts() {
-    if (INT0IF && INT0IE){
-        //Lcd_Write_String("Key press INT0");
-        get_input();
-        INT0IF = 0;
-    } else if (INT1IF && INT1IE) {
-        get_input();
+    /* Using all INTx for same job. Should use PORTB change interrupts*/
+    if (INT0IF 
+        || INT1IF
+        || INT2IF
+        || INT3IF){
+        //letters--;
+        keypad_isr();
+        INT0IF = 0;        
         INT1IF = 0;
-    }
-    else if (INT2IF && INT2IE) {
-        get_input();
         INT2IF = 0;
-    } else if (INT3IF && INT3IE) {
-        get_input();
         INT3IF = 0;
     }
-    letters--;
     return;
 }
+
 void main(void) {
     
     setup();
     Lcd_Write_String(MOTD_S);
     __delay_ms(1000);
     Lcd_Clear();
-    PORTB = 0xF0;
-    while(1);
+    PORTB = PORTB_NOMINAL;
+    
+    while(1) {
+        
+        if (new_char == 1) {
+            GIE = 0; //Disable interrupts
+            Lcd_Clear();
+            __delay_ms(50);
+            char buff[8]; // to be removed
+            sprintf(buff, "%d", letters);// to be removed 
+            Lcd_Write_String(buff);
+            new_char = 0;
+            GIE = 1; //Enable interrupts
+        }
+       
+    }
     return;
 }
 
@@ -77,7 +85,36 @@ void setup() {
     Lcd_Init();
 }
 
-void get_input() {
+void keypad_isr() {
+    /*
+     ISR Adds the inputed char into input[] in general scope.
+     */
+    char input_char = get_input_char();
+    for (int i = 0; i < sizeof(input) -1; i++){
+        if (input[i] == '\0') {
+            input[i] = input_char;
+            letters++;
+            new_char = 1;
+            break;
+        }
+    }
+    //input[PASSWD_LENGTH-letters] = input_char;
+    //Lcd_Write_String(input);
+      //__delay_ms(100);
+      //Lcd_Clear();
+//      if (letters == 0 ) {
+//          Lcd_Write_String("checking password");
+//          letters = PASSWD_LENGTH;
+//      }
+      //else Lcd_Write_String( (char)letters);
+      //Lcd_Write_Char('*');
+      
+//      __delay_ms(300);
+//      Lcd_Clear();
+}
+
+char get_input_char() {
+
     __delay_ms(100);
     char input_char = 0;
     PORTB = 0x10; // Test Row A
@@ -127,20 +164,7 @@ void get_input() {
         //ROW D Col 3
         input_char = '#';
     }
+     PORTB = PORTB_NOMINAL;
+     return input_char;
       
-      Lcd_Clear();
-      input[PASSWD_LENGTH-letters] = input_char;
-      Lcd_Write_String(input);
-      __delay_ms(100);
-      Lcd_Clear();
-      if (letters == 0 ) {
-          Lcd_Write_String("checking password");
-          letters = PASSWD_LENGTH;
-      }
-      //else Lcd_Write_String( (char)letters);
-      //Lcd_Write_Char('*');
-      
-      __delay_ms(300);
-      Lcd_Clear();
-      PORTB = 0xF0;
 }
